7f34a86e9d0eab3e147fbdca4c4e615c0d56dd15
Planeshifter <pburckhardt@outlook.com>
2015-02-24 01:57:41 -0500

    package.json, lib files, tests added

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var chai   = require('chai');
+var expect = chai.expect;
+var lib    = require("../lib/index.js");
+
+describe("findAnagrams()", function test(){
+
+});
+
+describe("findSimilarSounding()", function test(){
+
+});
+
+describe("StringWrapper()", function test(){
+
+});
