98ae5ae539136189f663d6b14a05aac6861b8600
Planeshifter <pburckhardt@outlook.com>
2015-02-24 14:23:50 -0500

    added I/O and finished similarSounding

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # anagrams
 find anagrams and similar sounding words
+
+## Unit Tests
+
+Run tests via the command `npm test`
--- test/test.js
+++ test/test.js
@@ -1,15 +1,43 @@
 var chai   = require('chai');
+chai.use(require('chai-things'));
 var expect = chai.expect;
 var lib    = require("../lib/index.js");
 
-describe("findAnagrams()", function test(){
+describe("findAnagrams()", function tests(){
+  
+  it("is a function", function test(){
+    expect(lib.findAnagrams).to.be.a("function");
+  });
 
+  it("correctly calculates anagrams for word array", function test(){
+
+    expect(lib.findAnagrams(["cat", "haste", "least", "steal"])).to.
+    include.something.that.deep.equals(["least", "steal"]);
+
+    expect(lib.findAnagrams(["cat", "haste", "least", "steal"])).to.
+    include.something.that.deep.equals(["cat"]);
+
+    expect(lib.findAnagrams(["cat", "haste", "least", "steal"])).to.
+    include.something.that.deep.equals(["haste"]);
+
+  });
 });
 
 describe("findSimilarSounding()", function test(){
+  it("is a function", function test(){
+    expect(lib.findSimilarSounding).to.be.a("function");
+  });
+  it("correctly groups together similar sounding words", function test(){
 
+  });
 });
 
-describe("StringWrapper()", function test(){
-
+describe("StringWrapper()", function tests(){
+  it("is a constructor function", function test(){
+    expect(lib.StringWrapper).to.be.a("function");
+  });
+  it("creates instances of StringWrapper", function test(){
+    var wrapped = new lib.StringWrapper("I am getting wrapped");
+    expect(wrapped instanceof lib.StringWrapper).to.be.equal(true);
+  });
 });
